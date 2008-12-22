90977d9353ab6dec61fba4e66427d979a02b4d77
Kousuke Ebihara <ebihara@tejimaya.com>
2008-12-22 06:24:55 +0000

    #3221:created the module OpenID

---
+++ apps/pc_frontend/modules/OpenID/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * OpenID actions.
+ *
+ * @package    OpenPNE
+ * @subpackage OpenID
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class OpenIDActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex($request)
+  {
+    $this->forward('default', 'module');
+  }
+}
